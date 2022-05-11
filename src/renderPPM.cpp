cb272ebfbef382cf9d8f04bdba056b49e65d9bde
gshaurya <gshaurya@umich.edu>
2022-05-11 15:39:40 -0400

    Blue to white gradient render

--- src/renderPPM.cpp
+++ src/renderPPM.cpp
@@ -2,11 +2,33 @@
 
 #include "vec3.hpp"
 #include "color.hpp"
+#include "ray.hpp"
+
+// Linear blend
+color ray_color(const ray& r){
+    const auto WHITE = color(1.0, 1.0, 1.0);
+    const auto BLUE = color(0.5, 0.7, 1.0);
+    vec3 unit_direction = unit_vector(r.direction());
+    // y is [-1, 1] t should be [0, 1]
+    auto t = 0.5 * (unit_direction[1] + 1);
+    return WHITE * (1 - t) + BLUE * t;
+}
 
 int main(){
     // Image
-    const int image_width = 256;
-    const int image_height = 256;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - (horizontal / 2) - (vertical / 2) - vec3(0, 0, focal_length);
 
     // Render
 
@@ -15,7 +37,10 @@ int main(){
     for (int j = 0; j < image_height; ++j){
         std::cerr << "Scanlines remaining " << image_height - j - 1 << '\n'; 
         for (int i = 0; i < image_width; ++i){
-            color pixel(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            auto x = double(i) / (image_width - 1);
+            auto y = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + horizontal * x + vertical * y - origin);
+            color pixel = ray_color(r);
             write_color(std::cout, pixel);
         }
     }
