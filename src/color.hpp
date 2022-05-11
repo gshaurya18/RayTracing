4cc3d6efb14295c2eb472ed24e9512aa6e51d26d
gshaurya <gshaurya@umich.edu>
2022-05-11 13:15:07 -0400

    Fixed typo

--- src/color.hpp
+++ src/color.hpp
@@ -5,7 +5,7 @@
 
 #include <iostream>
 
-void write_color(std::ostream& out, color pixel){
+void write_color(std::ostream& os, color pixel){
     // Write translated [0, 255] value
     const int ir = static_cast<int>(255.999 * pixel[0]);
     const int ig = static_cast<int>(255.999 * pixel[1]);
