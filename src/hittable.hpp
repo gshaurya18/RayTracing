2202c0f3dbbf4d6ff1d733ff3f47d5fb7d478132
gshaurya <gshaurya@umich.edu>
2022-05-11 19:42:22 -0400

    Virtual destruction for hittable objects

--- src/hittable.hpp
+++ src/hittable.hpp
@@ -21,6 +21,7 @@ struct hit_record{
 class hittable{
     public:
         virtual bool hit(const ray&r, double t_min, double t_max, hit_record& rec) const = 0;
+        virtual ~hittable() { }
 };
 
 #endif
