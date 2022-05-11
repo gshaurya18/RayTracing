6258e6764884280d7a1bf7c29e1ecbd1d68409b1
gshaurya <gshaurya@umich.edu>
2022-05-11 13:03:54 -0400

    Vector utility functions

--- src/vec3.hpp
+++ src/vec3.hpp
@@ -60,7 +60,52 @@ class vec3{
             return std::sqrt(norm_squared());
         }
 
+        // Utility Functions
+
+        // Print to output stream
+        friend std::ostream& operator<<(std::ostream& os, const vec3& vec){
+            os << vec.arr[0] << ' ' << vec.arr[1] << ' ' << vec.arr[2];
+            return os;
+        }
+
+        vec3 operator+(const vec3& other) const{
+            return vec3(arr[0] + other.arr[0], arr[1] + other.arr[1], arr[2] + other.arr[2]);
+        }
+
+        vec3 operator-(const vec3& other) const{
+            return vec3(arr[0] - other.arr[0], arr[1] - other.arr[1], arr[2] - other.arr[2]);
+        }
+
+        vec3 operator*(const vec3 &other) const{
+            return vec3(arr[0] * other.arr[0], arr[1] * other.arr[1], arr[2] * other.arr[2]);            
+        }
+
+        vec3 operator*(const double d) const{
+            return vec3(arr[0] * d, arr[1] * d, arr[2] * d);
+        }
+
+        vec3 operator/(const double d) const{
+            return *this * (1 / d);
+        }
+
+        // Vector dot product
+        friend double dot(const vec3& v1, const vec3& v2){
+            return v1.arr[0] * v2.arr[0]
+                 + v1.arr[1] * v2.arr[1]
+                 + v1.arr[2] * v2.arr[2];
+        }
+
+        // Vector cross product
+        friend vec3 cross(const vec3 v1, const vec3& v2){
+            return vec3(v1.arr[1] * v2.arr[2] - v1.arr[2] * v2.arr[1],
+                        v1.arr[2] * v2.arr[0] - v1.arr[0] * v2.arr[2],
+                        v1.arr[0] * v2.arr[1] - v1.arr[1] * v2.arr[0]);
+        }
 };
+
+inline vec3 unit_vector(const vec3& v){
+    return v / v.norm();
+}
 
 // Type aliases for vec3
 using point3 = vec3; // point in 3D space
