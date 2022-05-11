2f25b782bf812426fa6df64af0caf9c33b54f614
gshaurya <gshaurya@umich.edu>
2022-05-11 13:12:07 -0400

    color utility

---
+++ src/color.hpp
@@ -0,0 +1,17 @@
+#ifndef COLOR_HPP
+#define COLOR_HPP
+
+#include "vec3.hpp"
+
+#include <iostream>
+
+void write_color(std::ostream& out, color pixel){
+    // Write translated [0, 255] value
+    const int ir = static_cast<int>(255.999 * pixel[0]);
+    const int ig = static_cast<int>(255.999 * pixel[1]);
+    const int ib = static_cast<int>(255.999 * pixel[2]);
+
+    os << ir << ' ' << ig << ' ' << ib << '\n';
+}
+
+#endif
