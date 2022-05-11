7edeb5efc70de62c92f70eff48a25145dee08c56
gshaurya <gshaurya@umich.edu>
2022-05-11 19:00:57 -0400

    Front face tracking in hit record at geometric intersection

--- src/hittable.hpp
+++ src/hittable.hpp
@@ -8,6 +8,14 @@ struct hit_record{
     point3 p; // point where it hits a surface
     vec3 normal; // normal to the surface at the point
     double t; // t value for ray
+    bool front_face; // Determine ray hitting front or back at time of geometric intersection
+    // true if ray is outside the sphere
+    // Normal always points out
+    // if ray intersects from inside they are same directions
+    // if ray comes from outside they are opposite direction
+    inline void set_face_normal(const ray& r, const vec3& outward_normal){
+        normal = dot(r.direction(), outward_normal) < 0 ? outward_normal : -outward_normal; 
+    }
 };
 
 class hittable{
