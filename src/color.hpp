538981c08f713bf34bf3248ee0e08c399e19e0b0
gshaurya <gshaurya@umich.edu>
2022-05-12 11:49:35 -0400

    update write_color to handle multi sample

--- src/color.hpp
+++ src/color.hpp
@@ -5,11 +5,20 @@
 
 #include <iostream>
 
-void write_color(std::ostream& os, color pixel){
+void write_color(std::ostream& os, color pixel, int samples_per_pixel){
+    auto r = pixel[0];
+    auto g = pixel[1];
+    auto b = pixel[2];
+
+    // Scale cummulative value by number of samples
+    r /= samples_per_pixel;
+    g /= samples_per_pixel;
+    b /= samples_per_pixel;
+
     // Write translated [0, 255] value
-    const int ir = static_cast<int>(255.999 * pixel[0]);
-    const int ig = static_cast<int>(255.999 * pixel[1]);
-    const int ib = static_cast<int>(255.999 * pixel[2]);
+    const int ir = static_cast<int>(256 * clamp(r, 0.0, 0.999));
+    const int ig = static_cast<int>(256 * clamp(g, 0.0, 0.999));
+    const int ib = static_cast<int>(256 * clamp(b, 0.0, 0.999));
 
     os << ir << ' ' << ig << ' ' << ib << '\n';
 }
