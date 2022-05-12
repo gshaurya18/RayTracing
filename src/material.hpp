8fdcec9092a2f3ecbddb47ab4f4f7c37ca899138
gshaurya <gshaurya@umich.edu>
2022-05-12 16:09:40 -0400

    Lambertian material class

--- src/material.hpp
+++ src/material.hpp
@@ -2,14 +2,28 @@
 #define MATERIAL_HPP
 
 #include "util.hpp"
-
-struct hit_record;
+#include "hittable.hpp"
 
 class material{
     public:
         virtual bool scatter(
             const ray& ray_in, const hit_record& rec, color& attenuation, ray& scattered
         ) const = 0;
+};
+
+class lambertian: public material{
+    color albedo;
+    public:
+        lambertian (const color& c): albedo(c) { }
+
+        virtual bool scatter(
+            const ray& ray_in, const hit_record& rec, color& attenuation, ray& scattered
+        ) const override {
+            auto scatter_direction = rec.normal + random_unit_vector();
+            scattered = ray(rec.p, scatter_direction);
+            attenuation = albedo;
+            return true;
+        }
 };
 
 #endif
