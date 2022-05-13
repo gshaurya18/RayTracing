cfde1321c3528369e764647ecf57184854e9d7c7
gshaurya <gshaurya@umich.edu>
2022-05-13 15:50:05 -0400

    variable refractive index flass render

--- src/renderPPM.cpp
+++ src/renderPPM.cpp
@@ -48,10 +48,11 @@ int main(){
     auto material_left = make_shared<dielectric>(1.5);
     auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
-    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5, material_center)); // sphere
-    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, material_ground)); // ground
-    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right)); 
 
     // Camera
     camera cam;
