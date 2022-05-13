8b256fbb1ad07e9c42a02077874193a0ac4d0b83
gshaurya <gshaurya@umich.edu>
2022-05-13 15:45:00 -0400

    schlik's approximation

--- src/material.hpp
+++ src/material.hpp
@@ -47,6 +47,13 @@ class metal: public material{
 
 class dielectric: public material{
     double ri; // refractive index
+
+    static double reflectance(double cosine, double ref_idx){
+        // Use schlik's approximation for reflectance
+        auto r0 = (1 - ref_idx) / (1 + ref_idx);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+    }
     public:
         dielectric(double r): ri(r) { }
         
@@ -60,7 +67,15 @@ class dielectric: public material{
             double cos_t = std::fmin(dot(-unit_direction, rec.normal), 1.0);
             double sin_t = std::sqrt(1 - cos_t * cos_t);
 
-            vec3 direction = (refraction_ratio *  sin_t > 1.0) ? reflect(unit_direction, rec.normal) : refract(unit_direction, rec.normal, refraction_ratio);
+            bool cannot_refract = (refraction_ratio *  sin_t > 1.0);
+            vec3 direction;
+
+            if (cannot_refract || reflectance(cos_t, refraction_ratio) > random_double()){
+                direction = reflect(unit_direction, rec.normal);
+            }
+            else{
+                direction = refract(unit_direction, rec.normal, refraction_ratio);
+            }
 
             scattered = ray(rec.p, direction);
             return true;
